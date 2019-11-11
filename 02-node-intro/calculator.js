29aa75f165a737ca7fb2d24af7d52d778ad2c7c6
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-11 13:59:23 +0530

    modularity in node.js

---
+++ 02-node-intro/calculator.js
@@ -0,0 +1,22 @@
+var calculator = {
+    add(x,y){
+        return x + y;
+    },
+    subtract(x,y){
+        return x - y;
+    },
+    multiply(x,y){
+        return x * y;
+    },
+    divide(x,y){
+        return x / y;
+    }
+}
+
+var x = 100,
+    y = 50;
+    
+console.log(calculator.add(x,y));
+console.log(calculator.subtract(x, y));
+console.log(calculator.multiply(x, y));
+console.log(calculator.divide(x, y));
---
+++ 03-modularity/calculator-client.js
@@ -0,0 +1,7 @@
+var calculator = require('./calculator');
+
+var x = 100, y = 50;
+console.log(calculator.add(x, y));
+console.log(calculator.subtract(x, y));
+console.log(calculator.multiply(x, y));
+console.log(calculator.divide(x, y));
